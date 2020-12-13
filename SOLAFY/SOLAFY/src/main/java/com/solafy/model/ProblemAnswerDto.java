1743bf30cffd679df72a1ee1d4048a417a24087a
LEEAYOUNG <lay0711@naver.com>
2020-12-14 01:14:16 +0900

    [Update] ProblemDto&ProblemAnswerDto

--- SOLAFY/SOLAFY/src/main/java/com/solafy/model/ProblemAnswerDto.java
+++ SOLAFY/SOLAFY/src/main/java/com/solafy/model/ProblemAnswerDto.java
@@ -1,10 +1,62 @@
 package com.solafy.model;
-
+/**
+ * ProblemAnswerDto
+ * @author Lee AYoung
+ * @since 2020-12-13
+ */
 public class ProblemAnswerDto {
-	/* private 설정 
-	 * 타입 잘 보고 설정
-	 * toString 만들어주기
-	 * 생성자는 기본 + 모든 맴버변수 용 만들기(총 2개)
-	 * getter, setter만들어주기
-	 */
+	private int problemNo;
+	private String answer;
+	private String solution;
+	private String keyword;
+	
+	public ProblemAnswerDto() {
+		super();
+	}
+
+	public ProblemAnswerDto(int problemNo, String answer, String solution, String keyword) {
+		super();
+		this.problemNo = problemNo;
+		this.answer = answer;
+		this.solution = solution;
+		this.keyword = keyword;
+	}
+
+	public int getProblemNo() {
+		return problemNo;
+	}
+
+	public void setProblemNo(int problemNo) {
+		this.problemNo = problemNo;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public String getSolution() {
+		return solution;
+	}
+
+	public void setSolution(String solution) {
+		this.solution = solution;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	@Override
+	public String toString() {
+		return "ProblemAnswerDto [problemNo=" + problemNo + ", answer=" + answer + ", solution=" + solution
+				+ ", keyword=" + keyword + "]";
+	}
 }
--- SOLAFY/SOLAFY/src/main/java/com/solafy/model/ProblemDto.java
+++ SOLAFY/SOLAFY/src/main/java/com/solafy/model/ProblemDto.java
@@ -1,10 +1,114 @@
 package com.solafy.model;
-
+/**
+ * ProblemDto
+ * @author Lee AYoung
+ * @since 2020-12-13
+ */
 public class ProblemDto {
-	/* private 설정 
-	 * 타입 잘 보고 설정
-	 * toString 만들어주기
-	 * 생성자는 기본 + 모든 맴버변수 용 만들기(총 2개)
-	 * getter, setter만들어주기
-	 */
+	private int problemNo;
+	private String multipleChoice;
+	private String title;
+	private String uid;
+	private String contents;
+	private double starScore;
+	private int categorySmallNo;
+	private int type;
+	private String problemcol;
+	
+	public ProblemDto() {
+		super();
+	}
+
+	public ProblemDto(int problemNo, String multipleChoice, String title, String uid, String contents, double starScore,
+			int categorySmallNo, int type, String problemcol) {
+		super();
+		this.problemNo = problemNo;
+		this.multipleChoice = multipleChoice;
+		this.title = title;
+		this.uid = uid;
+		this.contents = contents;
+		this.starScore = starScore;
+		this.categorySmallNo = categorySmallNo;
+		this.type = type;
+		this.problemcol = problemcol;
+	}
+
+	public int getProblemNo() {
+		return problemNo;
+	}
+
+	public void setProblemNo(int problemNo) {
+		this.problemNo = problemNo;
+	}
+
+	public String getMultipleChoice() {
+		return multipleChoice;
+	}
+
+	public void setMultipleChoice(String multipleChoice) {
+		this.multipleChoice = multipleChoice;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public double getStarScore() {
+		return starScore;
+	}
+
+	public void setStarScore(double starScore) {
+		this.starScore = starScore;
+	}
+
+	public int getCategorySmallNo() {
+		return categorySmallNo;
+	}
+
+	public void setCategorySmallNo(int categorySmallNo) {
+		this.categorySmallNo = categorySmallNo;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public String getProblemcol() {
+		return problemcol;
+	}
+
+	public void setProblemcol(String problemcol) {
+		this.problemcol = problemcol;
+	}
+
+	@Override
+	public String toString() {
+		return "ProblemDto [problemNo=" + problemNo + ", multipleChoice=" + multipleChoice + ", title=" + title
+				+ ", uid=" + uid + ", contents=" + contents + ", starScore=" + starScore + ", categorySmallNo="
+				+ categorySmallNo + ", type=" + type + ", problemcol=" + problemcol + "]";
+	}
 }
