1865063237e2a5cce31b6e3267ad2e1ff4e77f0c
BUMSEOK SEO <sbseok12@gmail.com>
2020-12-14 03:00:23 +0900

    [UPDATE] FreeBoardDto 주석 수정, FreeReplyDto 추가

--- SOLAFY/SOLAFY/src/main/java/com/solafy/model/ReplyByFreeDto.java
+++ SOLAFY/SOLAFY/src/main/java/com/solafy/model/ReplyByFreeDto.java
@@ -1,10 +1,77 @@
 package com.solafy.model;
 
+/**
+ * ReplyByFreeDto
+ * 자유게시판 댓글 DTO
+ * @author BUMSEOK SEO
+ * @since 2020-12-14
+ * 
+ */
+
 public class ReplyByFreeDto {
-	/* private 설정 
-	 * 타입 잘 보고 설정
-	 * toString 만들어주기
-	 * 생성자는 기본 + 모든 맴버변수 용 만들기(총 2개)
-	 * getter, setter만들어주기
-	 */
+
+	private int replyNo;
+	private int articleNo;
+	private String contents;
+	private String uid;
+	private String regiTime;
+
+	public ReplyByFreeDto() {
+	}
+
+	public ReplyByFreeDto(int replyNo, int articleNo, String contents, String uid, String regiTime) {
+		super();
+		this.replyNo = replyNo;
+		this.articleNo = articleNo;
+		this.contents = contents;
+		this.uid = uid;
+		this.regiTime = regiTime;
+	}
+
+	public int getReplyNo() {
+		return replyNo;
+	}
+
+	public void setReplyNo(int replyNo) {
+		this.replyNo = replyNo;
+	}
+
+	public int getArticleNo() {
+		return articleNo;
+	}
+
+	public void setArticleNo(int articleNo) {
+		this.articleNo = articleNo;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getRegiTime() {
+		return regiTime;
+	}
+
+	public void setRegiTime(String regiTime) {
+		this.regiTime = regiTime;
+	}
+
+	@Override
+	public String toString() {
+		return "ReplyByFreeDto [replyNo=" + replyNo + ", articleNo=" + articleNo + ", contents=" + contents + ", uid="
+				+ uid + ", regiTime=" + regiTime + "]";
+	}
+
 }
