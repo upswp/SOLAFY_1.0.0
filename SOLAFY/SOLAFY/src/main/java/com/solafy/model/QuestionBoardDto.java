e7c4dacec3604f16ec8e736f68e02d9bf9861243
juheegg <eespacio95@gmail.com>
2020-12-14 00:55:34 +0900

    [update] 질문 게시판 - QuestionBoardDto, AnswerByQuestionDto 추가

--- SOLAFY/SOLAFY/src/main/java/com/solafy/model/QuestionBoardDto.java
+++ SOLAFY/SOLAFY/src/main/java/com/solafy/model/QuestionBoardDto.java
@@ -1,10 +1,119 @@
 package com.solafy.model;
 
+/**
+ * 
+ * @author Juhee Lee
+ * @since 2020-12-14
+ *
+ */
+
 public class QuestionBoardDto {
-	/* private 설정 
-	 * 타입 잘 보고 설정
-	 * toString 만들어주기
-	 * 생성자는 기본 + 모든 맴버변수 용 만들기(총 2개)
-	 * getter, setter만들어주기
-	 */
+
+	private int articleNo;
+	private String title;
+	private String contents;
+	private String uid;
+	private int problemNo;
+	private int point;
+	private String regiTime;
+	private boolean isGroup;
+	private int groupNo;
+
+	public QuestionBoardDto() {
+		super();
+	}
+
+	public QuestionBoardDto(int articleNo, String title, String contents, String uid, int problemNo, int point,
+			String regiTime, boolean isGroup, int groupNo) {
+		super();
+		this.articleNo = articleNo;
+		this.title = title;
+		this.contents = contents;
+		this.uid = uid;
+		this.problemNo = problemNo;
+		this.point = point;
+		this.regiTime = regiTime;
+		this.isGroup = isGroup;
+		this.groupNo = groupNo;
+	}
+
+	public int getArticleNo() {
+		return articleNo;
+	}
+
+	public void setArticleNo(int articleNo) {
+		this.articleNo = articleNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public int getProblemNo() {
+		return problemNo;
+	}
+
+	public void setProblemNo(int problemNo) {
+		this.problemNo = problemNo;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public String getRegiTime() {
+		return regiTime;
+	}
+
+	public void setRegiTime(String regiTime) {
+		this.regiTime = regiTime;
+	}
+
+	public boolean isGroup() {
+		return isGroup;
+	}
+
+	public void setGroup(boolean isGroup) {
+		this.isGroup = isGroup;
+	}
+
+	public int getGroupNo() {
+		return groupNo;
+	}
+
+	public void setGroupNo(int groupNo) {
+		this.groupNo = groupNo;
+	}
+
+	@Override
+	public String toString() {
+		return "QuestionBoardDto [articleNo=" + articleNo + ", title=" + title + ", contents=" + contents + ", uid="
+				+ uid + ", problemNo=" + problemNo + ", point=" + point + ", regiTime=" + regiTime + ", isGroup="
+				+ isGroup + ", groupNo=" + groupNo + "]";
+	}
+
 }
