6a60c58e9b9c63fa9eb5050516663b480aea35e5
upswp <tkddnsos@gmail.com>
2020-12-13 22:27:21 +0900

    problemSetDto commit

--- SOLAFY/SOLAFY/src/main/java/com/solafy/model/ProblemSetDto.java
+++ SOLAFY/SOLAFY/src/main/java/com/solafy/model/ProblemSetDto.java
@@ -7,4 +7,49 @@ public class ProblemSetDto {
 	 * 생성자는 기본 + 모든 맴버변수 용 만들기(총 2개)
 	 * getter, setter만들어주기
 	 */
+	
+	private String uid;
+	private String title;
+	private String regiTime;
+	
+	public ProblemSetDto() {
+		super();
+	}
+
+	public ProblemSetDto(String uid, String title, String regiTime) {
+		super();
+		this.uid = uid;
+		this.title = title;
+		this.regiTime = regiTime;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getRegiTime() {
+		return regiTime;
+	}
+
+	public void setRegiTime(String regiTime) {
+		this.regiTime = regiTime;
+	}
+
+	@Override
+	public String toString() {
+		return "ProblemSetDto [uid=" + uid + ", title=" + title + ", regiTime=" + regiTime + "]";
+	}
+	
 }
