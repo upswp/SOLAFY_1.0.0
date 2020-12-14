18ec5ad85adb2fdfa46de0ac66825ca5ff364326
BUMSEOK SEO <sbseok12@gmail.com>
2020-12-15 00:48:26 +0900

    [UPDATE] AnswerModifyBoardService.java

--- SOLAFY/SOLAFY/src/main/java/com/solafy/service/board/AnswerModifyBoardService.java
+++ SOLAFY/SOLAFY/src/main/java/com/solafy/service/board/AnswerModifyBoardService.java
@@ -1,5 +1,26 @@
 package com.solafy.service.board;
 
-public class AnswerModifyBoardService {
+import java.util.List;
 
+import com.solafy.model.AnswerModifyBoardDto;
+
+/**
+ * AnswerModifyBoardService
+ * 답안 수정 게시판 Interface
+ * @author BUMSEOK SEO
+ * @since 2020-12-15
+ */
+
+public interface AnswerModifyBoardService {
+	public int createAnswerModify(AnswerModifyBoardDto answerModifyDto);
+
+	public int updateAnswerModify(AnswerModifyBoardDto answerModifyDto);
+
+	public AnswerModifyBoardDto selectAnswerModifyByArticleNo();
+
+	public List<AnswerModifyBoardDto> selectAnswerModifyByUid();
+
+	public List<AnswerModifyBoardDto> selectAnswerModifyByUidSubMitter();
+
+	public List<AnswerModifyBoardDto> selectAnswerModifyByProblemNo();
 }
