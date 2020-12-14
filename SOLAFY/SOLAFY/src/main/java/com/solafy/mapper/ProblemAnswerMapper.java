dc7cd66d52932276b090b27d80b80abdc904d10b
LEEAYOUNG <lay0711@naver.com>
2020-12-14 19:32:58 +0900

    [Update] ProblemAnswerMapper & ProblemMapper 주석

--- SOLAFY/SOLAFY/src/main/java/com/solafy/mapper/ProblemAnswerMapper.java
+++ SOLAFY/SOLAFY/src/main/java/com/solafy/mapper/ProblemAnswerMapper.java
@@ -1,4 +1,5 @@
 package com.solafy.mapper;
+
 /**
  * ProblemAnswerMapper
  * @author Lee AYoung
@@ -7,12 +8,31 @@ package com.solafy.mapper;
 import com.solafy.model.ProblemAnswerDto;
 
 public interface ProblemAnswerMapper {
-	// 문제 정답 반환
+	/**
+	 * 문제 정답 반환
+	 * @param problemNo - 한 문제 번호
+	 * @return problemNo에 해당하는 ProblemAnswerDto 반환 - 전체 column
+	 */
 	ProblemAnswerDto selectProblemAnswer(int problemNo);
-	// 문제 정답 등록
+
+	/**
+	 * 문제 정답 등록
+	 * @param problemAnswerDto - 한 문제의 정답
+	 * @return int : 등록된 행의 개수 반환
+	 */
 	int insertProblemAnswer(ProblemAnswerDto problemAnswerDto);
-	// 문제 정답 수정
+
+	/**
+	 * 문제 정답 수정
+	 * @param problemAnswerDto - 한 문제의 정답
+	 * @return int : 수정된 행의 개수 반환
+	 */
 	int updateProblemAnswer(ProblemAnswerDto problemAnswerDto);
-	// 문제 정답 삭제
+
+	/**
+	 * 문제 정답 삭제
+	 * @param problemNo - 한 문제 번호
+	 * @return int : 삭제된 행의 개수 반환
+	 */
 	int deleteProblemAnswer(int problemNo);
 }
--- SOLAFY/SOLAFY/src/main/java/com/solafy/mapper/ProblemMapper.java
+++ SOLAFY/SOLAFY/src/main/java/com/solafy/mapper/ProblemMapper.java
@@ -1,4 +1,5 @@
 package com.solafy.mapper;
+import java.sql.SQLException;
 /**
  * ProblemMapper
  * @author Lee AYoung
@@ -10,26 +11,79 @@ import com.solafy.model.HashTagDto;
 import com.solafy.model.ProblemDto;
 
 public interface ProblemMapper {
-	// 문제집에 포함되는 문제리스트 반환
+	/**
+	 * 문제집에 포함되는 문제리스트 반환
+	 * @param problemSetNo - 한 문제집 번호
+	 * @return 문제집에 해당하는 ProblemDto의 List 반환 - problemNo, title, uid, starScore
+	 */
 	List<ProblemDto> selectProblemList(int problemSetNo);
-	// 문제하나 정보 반환(상세보기 용)
+	
+	/**
+	 * 문제하나 정보 반환(상세보기 용)
+	 * @param problemNo - 한 문제 번호
+	 * @return problemNo에 해당하는 ProblemDto 반환 - 전체 column
+	 */
 	ProblemDto selectProblem(int problemNo);
-	// 문제의 카테고리 반환 
 	
-	// 문제의 해시태그 반환 : hashtagmapping테이블에서 문제 번호를 받아 반환
+	/**
+	 * 문제의 카테고리 반환
+	 * @param 
+	 * @return 
+	 */
+	
+	/**
+	 * 문제의 해시태그 반환 : hashtagmapping테이블에서 문제 번호를 받아 반환
+	 * @param 
+	 * @return 
+	 */
 	List<HashTagDto> selectHashtagList(int problemNo);
-	// 해시태그로 문제 검색
-	List<ProblemDto> selectProblemByHashTag(String hashTag);
-	// 문제이름(일부)으로 검색
-	List<ProblemDto> selectProblemByName(String title);
-	// 카테고리를 이용한 문제 검색
 	
-	// 문제 평가하기(서비스단에서 계산해서 넣어줘야할 거 같은데 평균을 어찌줄까?)
+	/**
+	 * 해시태그로 문제 검색
+	 * @param 
+	 * @return 
+	 */ 
+	List<ProblemDto> selectProblemByHashTag(String hashTag);
+	 
+	/**
+	 * 문제이름(일부)으로 검색
+	 * @param 
+	 * @return 
+	 */ 
+	List<ProblemDto> selectProblemByName(String title);
+	 
+	/**
+	 * 카테고리를 이용한 문제 검색
+	 * @param 
+	 * @return 
+	 */ 
+	
+
+	/**
+	 * 문제 평가하기(서비스단에서 계산해서 넣어줘야할 거 같은데 평균을 어찌줄까?)
+	 * @param 
+	 * @return 
+	 */ 
 	int updateStarScore(double starScore);
-	// 문제 등록
+	
+	/**
+	 * 문제 등록
+	 * @param 
+	 * @return 
+	 */ 
 	int insertProblem(ProblemDto problemDto);
-	// 문제 수정
+	
+	/**
+	 * 문제 수정
+	 * @param 
+	 * @return 
+	 */ 
 	int updateProblem(ProblemDto problemDto);
-	// 문제 삭제
+	
+	/**
+	 * 문제 삭제
+	 * @param 
+	 * @return 
+	 */ 
 	int deleteProblem(int problemNo);
 }
