4376437ac0c69de55a6139eb9219c2e9307c66e3
KiHyeonYun <ykh094@naver.com>
2020-12-16 03:08:35 +0900

    2020.12.16 group ServiceImpl 작성

--- SOLAFY/SOLAFY/src/main/java/com/solafy/service/group/GroupServiceImpl.java
+++ SOLAFY/SOLAFY/src/main/java/com/solafy/service/group/GroupServiceImpl.java
@@ -1,11 +1,43 @@
 package com.solafy.service.group;
 
+import java.util.List;
+
 import com.solafy.model.GroupDto;
 
 public class GroupServiceImpl implements GroupService {
 
 	@Override
 	public boolean createGroup(GroupDto group) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<GroupDto> selectAllGroup() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GroupDto selectGroupByNo(int groupNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String CheckDuplicateName(String title) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateGroup(GroupDto group) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updatePermissionOfMember(String uid) {
 		// TODO Auto-generated method stub
 		return false;
 	}
