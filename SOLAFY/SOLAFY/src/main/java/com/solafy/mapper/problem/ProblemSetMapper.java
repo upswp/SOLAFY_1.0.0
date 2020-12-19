fa7d531b9308f0d60c65b8fe4c301aa7c7d8057f
upswp <tkddnsos@gmail.com>
2020-12-19 19:11:59 +0900

    ProblemSet Mapper/Service 문제집의 문제에 따른 CRUD 추가

--- SOLAFY/SOLAFY/src/main/java/com/solafy/mapper/problem/ProblemSetMapper.java
+++ SOLAFY/SOLAFY/src/main/java/com/solafy/mapper/problem/ProblemSetMapper.java
@@ -20,31 +20,31 @@ public interface ProblemSetMapper {
 
 	/**
 	 * 문제집 리스트를 전부 가져온다.
-	 * @return
+	 * @return ProblemSetDto 의 List - 번호, 작성자, 제목, 등록시간 포함
 	 * @throws SQLException
 	 */
 	public List<ProblemSetDto> selectAllProblemSet()throws SQLException;
 
 	/**
 	 * 문제집 번호에 해당하는 문제집을 가져온다.
-	 * @param problemSetNo
-	 * @return 
-	 * @throws SQLException
+	 * @param problemSetNo - int, 문제집 번호
+	 * @return ProblemSetDto의 - 번호, 작성자, 제목, 등록시간 포함
+	 * @throws SQLException 
 	 */
 	public ProblemSetDto selectProblemByNo(int problemSetNo)throws SQLException;
 
 	/**
 	 * 문제집 작성자에 해당하는 문제집을 가져온다.
-	 * @param uid
-	 * @return
+	 * @param uid - String, 검색된 uid
+	 * @return ProblemSetDto 의 List - 번호, 작성자, 제목, 등록시간 포함
 	 * @throws SQLException
 	 */
 	public List<ProblemSetDto> selectProblemByWriter(String uid)throws SQLException;
 	
 	/**
 	 * 검색된 키워드가 제목에 포함된 문제집을 가져온다.
-	 * @param title
-	 * @return
+	 * @param title - String, 검색된 제목 키워드
+	 * @return ProblemSetDto 의 List - 번호, 작성자, 제목, 등록시간 포함
 	 * @throws SQLException
 	 */
 	public List<ProblemSetDto> selectProblemByTitle(String title)throws SQLException;
@@ -52,7 +52,7 @@ public interface ProblemSetMapper {
 	/**
 	 * 문제집을 생성한다.
 	 * @param problemSet
-	 * @return
+	 * @return int , 등록된 문제집의 갯수 반환
 	 * @throws SQLException
 	 */
 	public int createProblemSet(ProblemSetDto problemSet)throws SQLException;
@@ -60,7 +60,7 @@ public interface ProblemSetMapper {
 	/**
 	 * 문제집의 정보를 수정한다.
 	 * @param problemSet
-	 * @return
+	 * @return int , 수정한 문제집의 갯수 반환
 	 * @throws SQLException
 	 */
 	public int updateProblemSet(ProblemSetDto problemSet)throws SQLException;
@@ -68,39 +68,39 @@ public interface ProblemSetMapper {
 	/**
 	 * 문제집을 삭제한다.
 	 * @param problemSetNo
-	 * @return
+	 * @return int , 삭제한 문제집의 갯수 반환
 	 * @throws SQLException
 	 */
 	public int deleteProblemSet(int problemSetNo)throws SQLException;
 	
 	/**
 	 * 문제집에서 문제들의 리스트를 등록한다.
-	 * @param map
-	 * @return
+	 * @param map - Map<String, Integer> - problemSetNo, problemNo 포함
+	 * @return 문제집의 리스트에 해당하는 ProblemSetDto를 가져온다. 
 	 * @throws SQLException
 	 */
-	public int selectProblemSetMapping(Map<String, Integer> map)throws SQLException;
+	public ProblemSetDto selectProblemSetMapping(Map<String, Integer> map)throws SQLException;
 	
 	/**
 	 * 문제집에서 문제들을 등록한다.
-	 * @param map
-	 * @return
+	 * @param map - Map<String, Integer> - problemSetNo, problemNo 포함
+	 * @return int 수정된 행의 갯수 반환
 	 * @throws SQLException
 	 */
 	public int createProblemSetMapping(Map<String, Integer> map)throws SQLException;
 	
 	/**
 	 * 문제집의 문제들의 문제 리스트를 수정한다. 
-	 * @param map
-	 * @return
+	 * @param map - Map<String, Integer> - problemSetNo, problemNo 포함
+	 * @return int 수정된 행의 갯수 반환
 	 * @throws SQLException
 	 */
 	public int updateProblemSetMapping(Map<String, Integer> map)throws SQLException;
 	
 	/**
 	 * 문제집의 문제들을 삭제한다.
-	 * @param map
-	 * @return
+	 * @param map - Map<String, Integer> - problemSetNo, problemNo 포함
+	 * @return int 삭제된 행의 갯수 반환
 	 * @throws SQLException
 	 */
 	public int deleteProblemSetMapping(Map<String, Integer> map)throws SQLException;
