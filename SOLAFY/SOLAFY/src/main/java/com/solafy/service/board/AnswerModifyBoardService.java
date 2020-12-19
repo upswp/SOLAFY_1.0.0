eb4c61eca4427a9a4265652e55e1f1f88dc71034
BUMSEOK SEO <sbseok12@gmail.com>
2020-12-19 19:43:58 +0900

    [UPDATE] AnswerModifyService.java 주석 수정

--- SOLAFY/SOLAFY/src/main/java/com/solafy/service/board/AnswerModifyBoardService.java
+++ SOLAFY/SOLAFY/src/main/java/com/solafy/service/board/AnswerModifyBoardService.java
@@ -12,15 +12,65 @@ import com.solafy.model.AnswerModifyBoardDto;
  */
 
 public interface AnswerModifyBoardService {
+	/**
+	* @Method Name : createAnswerModify
+	* @작성일 : 2020. 12. 19
+	* @작성자 : BUMSEOK SEO
+	* @param answerModifyDto
+	* @return
+	* @Method 설명 : 답안수정 게시글 등록
+	* @변경이력 :
+	*/
 	public int createAnswerModify(AnswerModifyBoardDto answerModifyDto);
 
+	/**
+	* @Method Name : updateAnswerModify
+	* @작성일 : 2020. 12. 19
+	* @작성자 : BUMSEOK SEO
+	* @param answerModifyDto
+	* @return
+	* @Method 설명 : 답안수정 게시글 수정
+	* @변경이력 :
+	*/
 	public int updateAnswerModify(AnswerModifyBoardDto answerModifyDto);
 
+	/**
+	* @Method Name : selectAnswerModifyByArticleNo
+	* @작성일 : 2020. 12. 19
+	* @작성자 : BUMSEOK SEO
+	* @return
+	* @Method 설명 : 게시글 번호로 답안수정 게시글 검색
+	* @변경이력 :
+	*/
 	public AnswerModifyBoardDto selectAnswerModifyByArticleNo();
 
+	/**
+	* @Method Name : selectAnswerModifyByUid
+	* @작성일 : 2020. 12. 19
+	* @작성자 : BUMSEOK SEO
+	* @return
+	* @Method 설명 : Uid로 답안수정 게시글 검색
+	* @변경이력 :
+	*/
 	public List<AnswerModifyBoardDto> selectAnswerModifyByUid();
 
+	/**
+	* @Method Name : selectAnswerModifyByUidSubMitter
+	* @작성일 : 2020. 12. 19
+	* @작성자 : BUMSEOK SEO
+	* @return
+	* @Method 설명 : 문제 제출자의 Uid로 답안수정 게시글 검색
+	* @변경이력 :
+	*/
 	public List<AnswerModifyBoardDto> selectAnswerModifyByUidSubMitter();
 
+	/**
+	* @Method Name : selectAnswerModifyByProblemNo
+	* @작성일 : 2020. 12. 19
+	* @작성자 : BUMSEOK SEO
+	* @return
+	* @Method 설명 : 문제 번호로 답안수정 게시글 검색
+	* @변경이력 :
+	*/
 	public List<AnswerModifyBoardDto> selectAnswerModifyByProblemNo();
 }
