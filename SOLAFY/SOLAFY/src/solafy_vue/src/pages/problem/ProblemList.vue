fcce6e1942a92e3ed38af76d4164f536f7ebaa81
Sangwoo Park <tkddnsos@gmail.com>
2020-12-27 21:20:35 +0900

    problemList 데이터 연동 완료

--- SOLAFY/SOLAFY/src/solafy_vue/src/pages/problem/ProblemList.vue
+++ SOLAFY/SOLAFY/src/solafy_vue/src/pages/problem/ProblemList.vue
@@ -21,15 +21,15 @@
           color="primary"
           :disable="loading"
           label="문제등록"
-          @click="ProblemSetList"
+          @click="ProblemCreate"
         />
       </div>
       <div class="col">
         <q-table
           title="문제 리스트"
-          :data="data"
+          :data="problems"
           :columns="columns"
-          row-key="id"
+          row-key="name"
           :filter="filter"
           :loading="loading"
         >
@@ -87,11 +87,11 @@
           </template>
           <template>
             <q-td>
-              <q-btn name="수정" @click="problemDetail(problem.no)">
+              <q-btn name="수정" @click="problemDetail(problems.problemNo)">
                 수정
               </q-btn>
               <template>
-                <q-btn name="삭제" @click="problemDelete(problem.no)">
+                <q-btn name="삭제" @click="problemDelete(problems.problemNo)">
                   삭제
                 </q-btn>
               </template>
@@ -124,7 +124,7 @@ export default {
       options: ["제목", "문제번호", "작성자"],
       columns: [
         {
-          name: "desc",
+          name: "problemNo",
           required: true,
           label: "문제번호",
           align: "left",
@@ -174,36 +174,50 @@ export default {
     };
   },
   methods: {
+    selectProblemList: function() {
+      this.loading = true;
+      Axios.get(`/problem`)
+        .then(response => {
+          this.problems = response.data;
+          console.log(this.problems[0]);
+        })
+        .catch(() => {
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
+    },
+    searchProblem: function() {
+      this.loading = true;
+      Axios.get(`/search/${this.options}/${this.keyword}`)
+        .then(response => {
+          this.problems = response.data;
+          console.log(this.problems[0]);
+        })
+        .catch(() => {
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
+    },
+
     // emulate fetching data from server
     problemList() {
       this.loading = true;
-      setTimeout(() => {
-        const index = Math.floor(Math.random() * (this.data.length + 1)),
-          row = this.original[Math.floor(Math.random() * this.original.length)];
-        if (this.data.length === 0) {
-          this.rowCount = 0;
-        }
-        row.id = ++this.rowCount;
-        const problemList = { ...row }; // extend({}, row, { name: `${row.name} (${row.__count})` })
-        this.data = [
-          ...this.data.slice(0, index),
-          problemList,
-          ...this.data.slice(index)
-        ];
-        this.loading = false;
-      }, 500);
     },
     ProblemSetList() {
       this.loading = true;
-      setTimeout(() => {
-        const index = Math.floor(Math.random() * this.data.length);
-        this.data = [
-          ...this.data.slice(0, index),
-          ...this.data.slice(index + 1)
-        ];
-        this.loading = false;
-      }, 500);
+    },
+    ProblemCreate() {
+      this.loading = true;
+    },
+    problemDetail(problmeNo) {
+      this.loading = true;
+    },
+    problemDelete(problmeNo) {
+      this.loading = true;
     }
+  },
+  created() {
+    this.selectProblemList();
   }
 };
 </script>
