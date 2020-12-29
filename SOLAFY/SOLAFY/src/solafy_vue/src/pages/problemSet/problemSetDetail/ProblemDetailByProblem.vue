93f15c62c17a42c44d9473779a4e59a9a35978fc
Sangwoo Park <tkddnsos@gmail.com>
2020-12-30 08:10:00 +0900

    problem Detail Problem Data 연동 완료

--- SOLAFY/SOLAFY/src/solafy_vue/src/pages/problemSet/problemSetDetail/ProblemDetailByProblem.vue
+++ SOLAFY/SOLAFY/src/solafy_vue/src/pages/problemSet/problemSetDetail/ProblemDetailByProblem.vue
@@ -2,7 +2,7 @@
   <div class="q-pa-md">
     <div class="column">
       <div class="col" id="header-title">
-        <h3>문제집 title</h3>
+        <h3 v-html="itemProblemSet.problemSet.title"></h3>
       </div>
       <div class="col">
         <div class="row">
@@ -16,45 +16,40 @@
       <div class="col">
         <div class="row">
           <div class="col-8">
-            <q-table
-              grid
-              :card-container-class="cardContainerClass"
-              title="Treats"
-              :data="data"
-              :columns="columns"
-              row-key="name"
-              hide-header
-              :pagination.sync="pagination1"
-              :rows-per-page-options="rowsPerPageOptions"
-            >
-              <template v-slot:item="props">
-                <div class="q-pa-xs col-xs-12 col-sm-6 col-md-4">
-                  <q-card>
-                    <q-card-section class="text-center">
-                      Calories for
-                      <br />
-                      <strong>{{ props.row.name }}</strong>
-                    </q-card-section>
-                    <q-separator />
-                    <q-card-section
-                      class="flex flex-center"
-                      :style="{ fontSize: props.row.calories + 'px' }"
-                    >
-                      <div>{{ props.row.calories }} g</div>
-                    </q-card-section>
-                  </q-card>
-                </div>
-              </template>
-            </q-table>
+            <!-- 문제 내용 -->
+
+            <!-- 마크다운으로 문제 내용 출력 -->
+            <div>
+              <q-markdown no-linkify>
+                {{ itemProblem.problem.contents }}
+              </q-markdown>
+            </div>
+            <!-- 객관식인 경우 선지 -->
+            <div class="answer" v-if="itemProblem.problem.type === 0">
+              <div v-for="(choice, index) in multipleChoice" :key="index">
+                <q-checkbox :val="index + 1" v-model="answerChecklist">
+                  <q-markdown>{{ index + 1 }}. {{ choice }}</q-markdown>
+                </q-checkbox>
+              </div>
+            </div>
+            <!-- 주관식인 경우 답안 입력란 -->
+            <div class="answer" v-else-if="itemProblem.problem.type === 1">
+              <q-input v-model="answerText" outlined dense />
+            </div>
+            <!-- 서술형인 경우 답안 입력란 -->
+            <div class="answer" v-else-if="itemProblem.problem.type === 2">
+              <q-input v-model="answerText" dense outlined autogrow />
+            </div>
           </div>
           <div class="col-4">
             <div class="q-pa-md">
               <q-table
                 title="문제 List"
-                :data="item.problemList"
+                :data="itemProblemSet.problemList"
                 :columns="listColumns"
                 hide-pagination
                 :pagination.sync="pagination"
+                @row-click="selectProblemByNo"
               />
               <div class="q-pa-lg flex flex-center">
                 <q-pagination
@@ -72,7 +67,7 @@
         <div class="row">
           <div class="col-10"></div>
           <div class="col-2">
-            <q-btn color="primary" label="답안제출" />
+            <q-btn color="primary" label="답안제출" @click="goToResult" />
           </div>
         </div>
       </div>
@@ -80,33 +75,13 @@
   </div>
 </template>
 <script>
-const deserts = ["Frozen Yogurt"];
-const data = [];
-
-deserts.forEach(name => {
-  for (let i = 0; i < 24; i++) {
-    data.push({
-      name: name + " (" + i + ")",
-      calories: 20 + Math.ceil(50 * Math.random())
-    });
-  }
-});
-
-data.sort(() => -1 + Math.floor(3 * Math.random()));
+import Axios from "axios";
+import routes from "src/router/routes";
 
 export default {
+  name: "problemSetListByProblem",
   data() {
     return {
-      filter: "",
-      pagination1: {
-        page: 1,
-        rowsPerPage: this.getItemsPerPage()
-      },
-      columns: [
-        { name: "name", label: "Name", field: "name" },
-        { name: "calories", label: "Calories (g)", field: "calories" }
-      ],
-      data,
       pagination: {
         sortBy: "desc",
         descending: false,
@@ -114,6 +89,7 @@ export default {
         rowsPerPage: 3
         // rowsNumber: xx if getting data from a server
       },
+
       listColumns: [
         {
           name: "문제번호",
@@ -129,7 +105,8 @@ export default {
           align: "center"
         }
       ],
-      item: {
+
+      itemProblemSet: {
         problemSet: {
           problemSetNo: 0,
           title: "",
@@ -138,53 +115,140 @@ export default {
         },
         problemList: []
       },
-      slide: "style",
-      lorem:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo."
+
+      multipleChoice: [],
+      itemProblem: {
+        problem: {
+          problemNo: 0,
+          multipleChoice: "",
+          title: "",
+          contents: "",
+          categoryNo: "",
+          type: 0,
+          regiTime: "",
+          nickname: ""
+        },
+
+        hashTag: []
+      },
+      answerChecklist: [],
+      answerText: "",
+      loading: true,
+      result: false
     };
   },
+  created() {
+    this.selectProblemSetByNo();
+  },
   methods: {
-    getItemsPerPage() {
-      const { screen } = this.$q;
-      if (screen.lt.sm) {
-        return 3;
+    selectProblemByNo: function(evt, row) {
+      //   this.showLoading();
+      Axios.get("problem/" + row.problemNo)
+        .then(Response => {
+          this.itemProblem = Response.data;
+          if (this.itemProblem.problem.multipleChoice != null) {
+            this.multipleChoice = this.itemProblem.problem.multipleChoice.split(
+              ","
+            );
+          }
+        })
+        .catch(error => {
+          this.$q.notify({
+            color: "negative",
+            textColor: "white",
+            icon: "error",
+            message: "조회 실패"
+          });
+          this.goToproblemList();
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    selectProblemSetByNo: function() {
+      //   this.showLoading();
+      Axios.get(
+        "/problem/problemset/problemSetSelectByNo/" +
+          this.$route.params.problemSetNo
+      )
+        .then(Response => {
+          console.log(Response.data);
+          this.itemProblemSet = Response.data;
+        })
+        .catch(error => {
+          this.$q.notify({
+            color: "negative",
+            textColor: "white",
+            icon: "error",
+            message: "조회 실패"
+          });
+          this.goToproblemList();
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    // 채점후 결과 페이지로 이동
+    goToResult() {
+      var type = this.itemProblem.problem.type;
+      if (
+        (type === 0 && this.answerChecklist == "") ||
+        ((type === 1 || type === 2) && this.answerText == "")
+      ) {
+        this.$q.notify({
+          color: "warning",
+          textColor: "white",
+          icon: "warning",
+          message: "정답을 입력해주세요"
+        });
+        return;
       }
-      if (screen.lt.md) {
-        return 6;
+      this.answerChecklist.sort();
+      // 정답 불러와서 비교
+      if (type === 0 || type === 1) {
+        this.loading = true;
+        Axios.get("problem/answer/" + this.itemProblem.problem.problemNo)
+          .then(Response => {
+            var answer = Response.data.answer;
+            if (type === 0 && answer === this.answerChecklist.toString()) {
+              this.result = true;
+            } else if (type === 1 && answer === this.answerText) {
+              this.result = true;
+            }
+            this.$router.push({
+              name: "ProblemResult",
+              params: {
+                problemNo: this.itemProblem.problem.problemNo,
+                result: this.result
+              }
+            });
+          })
+          .catch(error => {
+            this.$q.notify({
+              color: "negative",
+              textColor: "white",
+              icon: "error",
+              message: "채점 실패"
+            });
+          })
+          .finally(() => {
+            this.loading = false;
+          });
+      } else if (type === 2) {
+        this.$q.notify({
+          color: "positive",
+          textColor: "white",
+          icon: "done",
+          message: "주관식입니다"
+        });
       }
-      return 9;
     }
   },
   computed: {
     pagesNumber() {
       return Math.ceil(
-        this.item.problemList.length / this.pagination.rowsPerPage
+        this.itemProblemSet.problemList.length / this.pagination.rowsPerPage
       );
-    },
-    cardContainerClass() {
-      if (this.$q.screen.gt.xs) {
-        return (
-          "grid-masonry grid-masonry--" + (this.$q.screen.gt.sm ? "3" : "2")
-        );
-      }
-
-      return void 0;
-    },
-
-    rowsPerPageOptions() {
-      if (this.$q.screen.gt.xs) {
-        return this.$q.screen.gt.sm ? [3, 6, 9] : [3, 6];
-      }
-
-      return [3];
-    }
-  },
-  watch: {
-    vertical(val) {
-      this.navPos = val === true ? "right" : "bottom";
-    },
-    "$q.screen.name"() {
-      this.pagination.rowsPerPage = this.getItemsPerPage();
     }
   }
 };
