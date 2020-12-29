ef2c7ce0e68781e972eb8170011f206db3b6ea8d
Sangwoo Park <tkddnsos@gmail.com>
2020-12-29 21:37:04 +0900

    problemSetDetail-problems layout 설계 완료

---
+++ SOLAFY/SOLAFY/src/solafy_vue/src/pages/problemSet/problemSetDetail/ProblemDetailByProblem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-pa-md">
+    <div class="column" style="height: 150px">
+      <div class="col">
+        문제집 title 위치
+      </div>
+      <div class="col">
+        <div class="row">
+          <div class="col-10"></div>
+          <div class="col-2">버튼들 위치</div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="row">
+          <div class="col-8">문제 contents</div>
+          <div class="col-4">문제번호 contents</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
