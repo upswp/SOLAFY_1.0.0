07ee646dbfcfaedd1be7a41bf0c5df1e10b0985f
Sangwoo Park <tkddnsos@gmail.com>
2020-12-30 23:12:18 +0900

    problem result layout 설계 완료

---
+++ SOLAFY/SOLAFY/src/solafy_vue/src/pages/problem/ProblemResult.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="q-pa-md">
+    <div class="column">
+      <!-- 상단 제목 및 돌아가기 버튼 -->
+      <div class="col" id="header-title">
+        <h3>문제 타이틀</h3>
+      </div>
+      <!-- contents -->
+      <div class="col">
+        <div class="row justify-center">
+          <div class="col-12 col-md-auto" id="contents">
+            1111
+          </div>
+        </div>
+        <br />
+        <div class="row justify-center">
+          <div class="col-12 col-md-auto" id="contents">
+            <div class="q-pa-md">
+              2222222
+            </div>
+            <q-separator spaced />
+          </div>
+        </div>
+      </div>
+      <!-- 하단 문제집 삭제,수정, 시작하기 버튼 -->
+      <div class="col">
+        <div class="row">
+          <div class="col-10"></div>
+          <div class="col-2">
+            <q-btn color="primary" label="돌아가기" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
