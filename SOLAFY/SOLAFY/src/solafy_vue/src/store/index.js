5987c0d039d6f0bf4d208973077268f87cfbeec3
BUMSEOK SEO <sbseok12@gmail.com>
2020-12-31 02:40:35 +0900

    [ADD] mutation-type.js [UPDATE] store/index.js board components에서 vuex저장소를 사용하기 위한 setting

--- SOLAFY/SOLAFY/src/solafy_vue/src/store/index.js
+++ SOLAFY/SOLAFY/src/solafy_vue/src/store/index.js
@@ -1,9 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+
+// BOARD쪽 사용을 위한 요소 추가
+import { SETBOARDCOLUMN, SETBOARDSEARCHKEYWORDS } from "./mutation-type";
 
 // import example from './module-example'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 /*
  * If not building with SSR mode, you can
@@ -14,8 +17,40 @@ Vue.use(Vuex)
  * with the Store instance.
  */
 
-export default function (/* { ssrContext } */) {
+export default function(/* { ssrContext } */) {
   const Store = new Vuex.Store({
+    state: {
+      boardColumns: [],
+      boardSearchKeywords: []
+    },
+    // 데이터인 state를 바로 반환할 수 있도록 getters 이용
+    getters: {
+      getBoardColumns: state => {
+        return state.boardColumns;
+      },
+      getBoardSearchKeywords: state => {
+        return state.boardSearchKeywords;
+      }
+    },
+    // state를 변경할 수 있는 유일한 수단인 mutations
+    mutations: {
+      [SETBOARDCOLUMN](state, payload) {
+        state.boardColumns = payload;
+      },
+      [SETBOARDSEARCHKEYWORDS](state, payload) {
+        state.boardSearchKeywords = payload;
+      }
+    },
+    // 비동기나 순차적 call이 필요할 때 사용
+    actions: {
+      [SETBOARDCOLUMN](state, payload) {
+        state.boardColumns = payload;
+      },
+      [SETBOARDSEARCHKEYWORDS](state, payload) {
+        state.boardSearchKeywords = payload;
+      }
+    },
+
     modules: {
       // example
     },
@@ -23,7 +58,7 @@ export default function (/* { ssrContext } */) {
     // enable strict mode (adds overhead!)
     // for dev mode only
     strict: process.env.DEBUGGING
-  })
+  });
 
-  return Store
+  return Store;
 }
---
+++ SOLAFY/SOLAFY/src/solafy_vue/src/store/mutation-type.js
@@ -0,0 +1,4 @@
+// 나중에 API콜이 엄청나게 많아졌을 때 비슷비슷한 이름이 여럿 생길 수도 있다.
+
+export const SETBOARDSEARCHKEYWORDS = "SETBOARDSEARCHKEYWORDS";
+export const SETBOARDCOLUMNS = "SETBOARDCOLUMNS";
