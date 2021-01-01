53e6c4dbc38ac813bda31b8ab404a1e68061e61c
juheegg <eespacio95@gmail.com>
2021-01-02 01:46:50 +0900

    [update] 질문게시판 - 답변 DTO와 mapper 수정 - 컴포넌트 재사용을 용이하게 하기 위해 field 명 수정

--- SOLAFY/SOLAFY/src/main/java/com/solafy/model/AnswerByQuestionDto.java
+++ SOLAFY/SOLAFY/src/main/java/com/solafy/model/AnswerByQuestionDto.java
@@ -5,14 +5,15 @@ package com.solafy.model;
  * @author Juhee Lee
  * @since 2020-12-14
  *
- * @변경이력
- * 20-12-28 닉네임 추가
+ * @변경이력 
+ * 20-12-28 [이주희] 닉네임 추가 
+ * 21-01-02 [이주희] 컴포넌트 재사용을 용이하게 하기 위해 field 명 수정
  */
 
 public class AnswerByQuestionDto {
 
+	private int replyNo;
 	private int articleNo;
-	private int questionNo;
 	private String contents;
 	private String uid;
 	private String regiTime;
@@ -22,15 +23,23 @@ public class AnswerByQuestionDto {
 		super();
 	}
 
-	public AnswerByQuestionDto(int articleNo, int questionNo, String contents, String uid, String regiTime,
+	public AnswerByQuestionDto(int replyNo, int articleNo, String contents, String uid, String regiTime,
 			String nickname) {
 		super();
+		this.replyNo = replyNo;
 		this.articleNo = articleNo;
-		this.questionNo = questionNo;
 		this.contents = contents;
 		this.uid = uid;
 		this.regiTime = regiTime;
 		this.nickname = nickname;
+	}
+
+	public int getReplyNo() {
+		return replyNo;
+	}
+
+	public void setReplyNo(int replyNo) {
+		this.replyNo = replyNo;
 	}
 
 	public int getArticleNo() {
@@ -39,14 +48,6 @@ public class AnswerByQuestionDto {
 
 	public void setArticleNo(int articleNo) {
 		this.articleNo = articleNo;
-	}
-
-	public int getQuestionNo() {
-		return questionNo;
-	}
-
-	public void setQuestionNo(int questionNo) {
-		this.questionNo = questionNo;
 	}
 
 	public String getContents() {
@@ -83,7 +84,7 @@ public class AnswerByQuestionDto {
 
 	@Override
 	public String toString() {
-		return "AnswerByQuestionDto [articleNo=" + articleNo + ", questionNo=" + questionNo + ", contents=" + contents
+		return "AnswerByQuestionDto [replyNo=" + replyNo + ", articleNo=" + articleNo + ", contents=" + contents
 				+ ", uid=" + uid + ", regiTime=" + regiTime + ", nickname=" + nickname + "]";
 	}
 
