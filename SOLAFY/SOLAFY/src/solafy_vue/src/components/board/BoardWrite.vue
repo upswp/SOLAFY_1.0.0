5c2f22a498c75b7ab193887f6129048bfcf8ce9a
BUMSEOK SEO <sbseok12@gmail.com>
2021-01-03 12:23:31 +0900

    [UPDATE] 게시글 등록 부분에서 문제검색 기능 추가

--- SOLAFY/SOLAFY/src/solafy_vue/src/components/board/BoardWrite.vue
+++ SOLAFY/SOLAFY/src/solafy_vue/src/components/board/BoardWrite.vue
@@ -3,9 +3,7 @@
   <q-page class="q-pa-md">
     <q-card flat bordered>
       <q-item>
-        <q-item-section>
-          게시글 작성
-        </q-item-section>
+        <q-item-section> 게시글 작성 </q-item-section>
       </q-item>
       <q-item>
         <q-item-section avatar>
@@ -28,10 +26,93 @@
           /></q-item-label>
           <!-- TODO: 닉네임은 자동으로 불러옴 -->
           <q-item-label caption>
+            아아아{{ article.uid }}
             닉네임 : 파베에서 로그인 정보 가져오기
           </q-item-label>
         </q-item-section>
       </q-item>
+      <q-separator />
+
+      <!-- 게시글에서 다룰 문제 검색 부분 구현 시작 -->
+      <template
+        v-if="
+          this.boardType === `answermodify` || this.boardType === `question`
+        "
+      >
+        <q-card-section v-if="showProblemSearchFlag">
+          <q-table
+            :title="message"
+            :data="problems"
+            :columns="columns"
+            row-key="name"
+            :loading="loading"
+            @row-click="selectProblem"
+          >
+            <template v-slot:top-right>
+              <q-input
+                bottom-slots
+                v-model="keyword"
+                label="검색어를 입력해주세요"
+                counter
+                maxlength="12"
+                @keyup.enter="search"
+                style="min-width:400px"
+              >
+                <template v-slot:before>
+                  <q-select
+                    id="selectbox"
+                    filled
+                    v-model="selection"
+                    :options="options"
+                    label="검색조건"
+                    style="min-width:120px"
+                  />
+                </template>
+                <template v-slot:append>
+                  <q-icon
+                    v-if="keyword !== ''"
+                    name="close"
+                    @click="keyword = ''"
+                    class="cursor-pointer"
+                  />
+                  <q-icon name="search" @click="search" />
+                </template>
+              </q-input>
+            </template>
+            <template v-slot:no-data>
+              <div class="full-width row flex-center text-accent q-gutter-sm">
+                <q-icon size="2em" name="volume_off" />
+                <b style="font-size:15px"> 문제를 검색해주세요 </b>
+              </div>
+            </template>
+          </q-table>
+        </q-card-section>
+        <q-card-section v-else>
+          <q-separator />
+          문제 작성자 : {{ problemInfo.nickname }}<br /><br />
+          {{ problemInfo.problemNo }}번째 문제 내용<br />
+          <strong>{{ problemInfo.contents }}</strong
+          ><br />
+
+          <template v-if="problemInfo.type === 0">
+            <div v-for="(choice, index) in multipleChoice" :key="index">
+              {{ index + 1 }} : {{ choice }}
+            </div>
+          </template>
+          <!-- 검색 초기화 버튼 시작 -->
+          <template>
+            <q-btn
+              outline
+              color="black"
+              label="문제 다시 선택하기"
+              flat
+              @click="showProblemSearchFlag = true"
+            />
+          </template>
+          <!-- 검색 초기화 버튼 끝 -->
+        </q-card-section>
+      </template>
+      <!-- 게시글에서 다룰 문제 검색 부분 구현 끝 -->
       <q-separator />
       <q-card-section>
         <q-card-section class="col-4">
@@ -40,14 +121,13 @@
             v-model="article.contents"
             filled
             type="textarea"
-            label="내용"
-            aria-placeholder="내용을 입력해주세요"
+            label="내용을 입력해주세요"
             autogrow
           />
         </q-card-section>
       </q-card-section>
       <!-- TODO: 공지사항 사용하는 게시판 한정, 관리자 권한의 인원만 사용하도록 함 -->
-      <q-card-section align="right"
+      <q-card-section align="right" v-if="this.boardType === `free`"
         ><q-checkbox v-model="article.notice" label="공지사항 여부" />
       </q-card-section>
       <q-separator />
@@ -61,11 +141,23 @@
 </template>
 <script>
 import Axios from "axios";
+
+import { firebaseAuth, firebaseSt } from "boot/firebase";
+import { SessionStorage, uid } from "quasar";
+import { mapState } from "vuex";
+
 export default {
   data() {
     return {
       // 입력내용이 저장되는 값들
       article: {
+        //추가 정보
+        // TODO: 현재의 UID를 가져와야한다. 힘들면 nickname이라도
+        uid: SessionStorage.getItem("loginUser").uid,
+        problemNo: "",
+        uid_submitter: "",
+
+        //기존 자유게시판 정보
         title: "",
         contents: "",
         isNotice: false,
@@ -73,13 +165,64 @@ export default {
       },
       // 게시판 형식 값을
       // store에서 가져와 data에 저장
-      boardType: this.$store.state.boardType
+      boardType: this.$store.state.boardType,
+
+      // 문제검색을 위한 데이터
+      selection: "제목",
+      options: ["제목", "문제번호", "작성자"],
+      keyword: "",
+      columns: [
+        {
+          name: "problemNo",
+          required: true,
+          label: "문제번호",
+          align: "left",
+          field: row => row.problemNo,
+          format: val => `${val}`,
+          sortable: true,
+          style: "width = 10px"
+        },
+        {
+          name: "nickname",
+          label: "작성자",
+          required: true,
+          align: "left",
+          field: row => row.nickname,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: "title",
+          label: "제목",
+          required: true,
+          align: "left",
+          field: row => row.title,
+          format: val => `${val}`,
+          sortable: true
+        },
+        {
+          name: "regiTime",
+          label: "등록일자",
+          required: true,
+          align: "left",
+          field: row => row.regiTime,
+          format: val => `${val}`,
+          sortable: true
+        }
+      ],
+      problems: [],
+      loading: false,
+      showProblemSearchFlag: true,
+      problemInfo: [],
+      message: ""
     };
   },
   methods: {
     // 게시글 생성
     createArticle: function() {
       var successFlag = true;
+      this.article.uid = "DFEIJC23WOSKXCNSWQ";
+      this.article.uid_submitter = this.problemInfo.uid;
 
       // 게시글 제목이 비어있다면
       if (this.article.title === null || this.article.title == "") {
@@ -101,8 +244,6 @@ export default {
 
       // 제목과 내용이 비어있지 않다면!
       if (successFlag) {
-        // TODO: 현재의 UID를 가져와야한다. 힘들면 nickname이라도
-        this.article.uid = "DFEIJC23WOSKXCNSWQ";
         // 입력한 값을 묶어서 던진다
         Axios.post(`/${this.boardType}/createArticle`, this.article)
           .then(response => {
@@ -137,6 +278,60 @@ export default {
       this.$router.push({
         name: `${this.boardType}-board-list`
       });
+    },
+
+    // 문제 검색을 위한 메서드 시작
+    //문제 검색 및 검색 예외처리
+    search: function() {
+      // console.log("helloworld");
+      console.log(typeof this.article.uid, "hello");
+      console.log(SessionStorage.getItem("loginUser").uid);
+      this.loading = true;
+      Axios.get(`/problem/search/${this.selection}/${this.keyword}`)
+        .then(response => {
+          this.problems = response.data;
+          if (this.problems.length === 0) {
+            this.$q.notify({
+              color: "red-6",
+              textColor: "white",
+              icon: "warning",
+              message: "조회 실패"
+            });
+          }
+          console.log(this.problems[0]);
+        })
+        .catch(() => {
+          this.$q.notify({
+            color: "red-6",
+            textColor: "white",
+            icon: "warning",
+            message: "조회 실패"
+          });
+        })
+        .finally(() => (this.loading = false));
+    },
+    selectProblem: function(evt, row) {
+      Axios.get(`problem/${row.problemNo}`)
+        .then(response => {
+          this.problemInfo = response.data["problem"];
+          console.log(this.problemInfo);
+          if (this.problemInfo.multipleChoice !== null)
+            this.multipleChoice = this.problemInfo.multipleChoice.split(",");
+          this.article.problemNo = this.problemInfo.problemNo;
+          this.article.uid_submitter = this.problemInfo.uid;
+          console.log(this.article.problemNo);
+          console.log(this.article.uid_submitter);
+        })
+        .catch(error => console.log(error))
+        .finally(() => (this.loading = false));
+      this.showProblemSearchFlag = false;
+    }
+  },
+  mounted() {
+    if (this.boardType === "answermodify") {
+      this.message = "수정을 요청할 문제 검색";
+    } else if (this.boardType === "question") {
+      this.message = "질문할 문제 검색";
     }
   }
 };
