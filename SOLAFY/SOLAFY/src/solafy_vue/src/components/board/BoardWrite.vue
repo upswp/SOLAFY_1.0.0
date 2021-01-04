76997b33432b4dae5e7aefb0bbf5b322b68e10e2
BUMSEOK SEO <sbseok12@gmail.com>
2021-01-04 20:15:39 +0900

    [UPDATE] BoardWrite.vue 코드정리, 프로필 사진 표시 완료

--- SOLAFY/SOLAFY/src/solafy_vue/src/components/board/BoardWrite.vue
+++ SOLAFY/SOLAFY/src/solafy_vue/src/components/board/BoardWrite.vue
@@ -3,14 +3,12 @@
   <q-page class="q-pa-md">
     <q-card flat bordered>
       <q-item>
-        <q-item-section> 게시글 작성 </q-item-section>
+        <q-item-section> <b>게시글 작성</b> </q-item-section>
       </q-item>
       <q-item>
         <q-item-section avatar>
           <q-avatar>
-            <!-- TODO: 프로필사진 가져오기 -->
-            <!-- <img src="https://cdn.quasar.dev/img/boy-avatar.png" /> -->
-            <img id="profileImg" />
+            <img :src="profileImageUrl" />
           </q-avatar>
         </q-item-section>
 
@@ -24,12 +22,7 @@
               aria-placeholder="제목을 입력해주세요"
               dense
           /></q-item-label>
-          <!-- TODO: 닉네임은 자동으로 불러옴 -->
-          <q-item-label caption>
-            {{ article.uid }}
-            {{ article.nickname }}
-            닉네임 : 파베에서 로그인 정보 가져오기
-          </q-item-label>
+          <q-item-label caption> 작성자 : {{ article.nickname }} </q-item-label>
         </q-item-section>
       </q-item>
       <q-separator />
@@ -58,7 +51,7 @@
                 label="검색어를 입력해주세요"
                 counter
                 maxlength="12"
-                @keyup.enter="search"
+                @keyup.enter="selectProblemByKeyword"
                 style="min-width:400px"
               >
                 <template v-slot:before>
@@ -78,7 +71,7 @@
                     @click="keyword = ''"
                     class="cursor-pointer"
                   />
-                  <q-icon name="search" @click="search" />
+                  <q-icon name="search" @click="selectProblemByKeyword" />
                 </template>
               </q-input>
             </template>
@@ -100,9 +93,11 @@
         <q-card-section v-else-if="isProblemSelected">
           <!-- 문제 정보 표시 시작 -->
           <q-separator />
-          문제 작성자 : {{ problemInfo.nickname }}<br /><br />
-          {{ problemInfo.problemNo }}번째 문제 내용<br />
-          <strong>{{ problemInfo.contents }}</strong
+          {{ problemInfo.problemNo }}번 문제<br />
+          <span style="font-size:0.5em"
+            >문제 작성자 : {{ problemInfo.nickname }}</span
+          ><br />
+          <b>{{ problemInfo.contents }}</b
           ><br />
 
           <template v-if="problemInfo.type === 0">
@@ -146,7 +141,7 @@
       <q-separator />
       <q-card-actions align="right">
         <q-btn color="primary" label="글 등록" @click="createArticle" />
-        <q-btn color="red" label="취소" @click="goToBoard" />
+        <q-btn color="red" label="취소" @click="goToBoardList" />
       </q-card-actions>
     </q-card>
   </q-page>
@@ -162,33 +157,38 @@ import { mapState } from "vuex";
 export default {
   data() {
     return {
-      // 입력내용이 저장되는 값들
+      loading: false,
+      // 프로필 사진 주소값 저장 변수
+      profileImageUrl: "",
+      // 게시판 형식 값을 store에서 가져와 저장하는 변수
+      boardType: this.$store.state.boardType,
+      // 문제 검색 테이블 안내 문구 변수
+      message: "",
       article: {
-        //추가 정보
-        // TODO: 현재의 UID를 가져와야한다. 힘들면 nickname이라도
-        // uid: firebaseAuth.currentUser.uid,
         uid: SessionStorage.getItem("loginUser").uid,
         nickname: SessionStorage.getItem("loginUser").nickname,
         problemNo: "",
         uid_submitter: "",
-
-        //기존 자유게시판 정보
         title: "",
         contents: "",
         isNotice: false,
         groupNo: 1
       },
-      profileUrl: "https://cdn.quasar.dev/img/boy-avatar.png",
-      // 게시판 형식 값을
-      // store에서 가져와 data에 저장
-      boardType: this.$store.state.boardType,
 
-      loading: false,
-      message: "",
-      // 문제검색을 위한 데이터 시작
+      // * 문제검색을 위한 데이터 시작
+      // 문제 선택 여부 flag
+      isProblemSelected: false,
+      // 문제 검색 옵션 선택 저장 변수
       selection: "제목",
-      options: ["제목", "문제번호", "작성자"],
+      // 문제 검색 키워드 변수
       keyword: "",
+      // 문제 검색 옵션 배열
+      options: ["제목", "문제번호", "작성자"],
+      // 문제 검색 테이블에 표시할 문제 모음
+      problems: [],
+      // 선택한 문제 정보 저장 변수
+      problemInfo: [],
+      // 문제 검색용 테이블 컬럼
       columns: [
         {
           name: "problemNo",
@@ -227,54 +227,51 @@ export default {
           format: val => `${val}`,
           sortable: true
         }
-      ],
-      problems: [],
-      isProblemSelected: false,
-      problemInfo: []
-      //문제 검색을 위한 데이터 끝
+      ]
+      // * 문제 검색을 위한 데이터 끝
     };
   },
   methods: {
-    // 게시글 생성
+    /**
+     * @Method설명 : 게시글 작성 요청 메소드
+     * @변경이력 :
+     */
     createArticle: function() {
       var successFlag = true;
-      this.article.uid = "DFEIJC23WOSKXCNSWQ";
-      this.article.uid_submitter = this.problemInfo.uid;
-      console.log(this.article.uid_submitter);
 
-      // 게시글 제목이 비어있다면
+      // 게시글 제목이 비어 있을 때
       if (this.article.title === null || this.article.title == "") {
         this.$q.notify({
-          message: "*제목* 비어있으면 안돼요!",
+          message: "*제목* 비어 있으면 안돼요!",
           color: "orange"
         });
         successFlag = false;
       }
 
-      // 게시글 내용이 비어있다면
+      // 게시글 내용이 비어 있을 때
       if (this.article.contents == null || this.article.contents == "") {
         this.$q.notify({
-          message: "*내용* 비어있으면 안돼요!",
+          message: "*내용* 비어 있으면 안돼요!",
           color: "orange"
         });
         successFlag = false;
       }
 
-      // 제목과 내용이 비어있지 않다면!
+      // 제목과 내용이 채워져 있을 때
       if (successFlag) {
         // 입력한 값을 묶어서 던진다
         Axios.post(`/${this.boardType}/createArticle`, this.article)
           .then(response => {
             if (response.data === "success") {
               // 목록으로 돌아감
-              this.goToBoard();
+              this.goToBoardList();
               // 등록 성공알림
               this.$q.notify({
                 message: "게시물 등록 성공",
                 color: "green"
               });
             } else {
-              // 실패
+              // 게시물 등록 실패
               this.$q.notify({
                 message: "게시물 등록 실패",
                 color: "red"
@@ -290,25 +287,18 @@ export default {
       }
     },
 
-    // 목록으로 돌아가는 메서드.
-    // 해당 게시판에 해당하는 리스트로 날아간다
-    goToBoard: function() {
-      this.$router.push({
-        name: `${this.boardType}-board-list`
-      });
-    },
-
-    // 문제 검색을 위한 메서드 시작
-    //문제 검색 및 검색 예외처리
-    search: function() {
-      // TODO: 로그인 정보 불러오기 점검
-      console.log(typeof this.article.uid, "hello");
-      console.log(firebaseAuth.currentUser.uid);
-      console.log(firebaseAuth.currentUser.nickname);
+    /**
+     * @Method설명 : 검색 옵션에 해당하는 문제 검색 및 검색 예외처리 메소드
+     * @변경이력 :
+     */
+    selectProblemByKeyword: function() {
       this.loading = true;
+      // 검색 옵션과 검색 키워드에 맞는 문제를 얻어 옴
       Axios.get(`/problem/search/${this.selection}/${this.keyword}`)
         .then(response => {
+          // 반환된 문제 배열을 problems에 저장한다
           this.problems = response.data;
+          // 반환된 문제 배열의 크기가 0일 때
           if (this.problems.length === 0) {
             this.$q.notify({
               color: "red-6",
@@ -317,7 +307,6 @@ export default {
               message: "조회 실패"
             });
           }
-          console.log(this.problems[0]);
         })
         .catch(() => {
           this.$q.notify({
@@ -329,59 +318,72 @@ export default {
         })
         .finally(() => (this.loading = false));
     },
-    // 문제를 선택했을때 메서드
+
+    /**
+     * @Method설명 : 선택한 문제 번호에 해당하는 문제를 가져오는 메소드
+     * @변경이력 :
+     */
     selectProblem: function(evt, row) {
       Axios.get(`problem/${row.problemNo}`)
         .then(response => {
+          // 반환된 값을 problemInfo에 저장한다
           this.problemInfo = response.data["problem"];
-          console.log(this.problemInfo);
+          // 선택지가 존재하는 문제일 때
           if (this.problemInfo.multipleChoice !== null)
+            // 콤마로 구분되어 있는 선택지를 multipleChoice에 저장
             this.multipleChoice = this.problemInfo.multipleChoice.split(",");
+
+          // 게시글에 필요한 문제번호, 문제 제출자 정보를 저장
           this.article.problemNo = this.problemInfo.problemNo;
           this.article.uid_submitter = this.problemInfo.uid;
-          console.log(this.article.problemNo);
         })
         .catch(error => console.log(error))
         .finally(() => (this.loading = false));
       this.isProblemSelected = true;
     },
-    getProfileImageUrl: function() {
-      var returnValue;
-      var hello = firebaseSt
+
+    /**
+     * @Method설명 : 로그인된 사용자의 프로필 이미지를 가져오는 메소드
+     * @변경이력 :
+     */
+    getProfileImageUrl: function(uid) {
+      firebaseSt
         .ref()
-        .child("profileimg/" + this.article.uid)
+        .child("profileimg/" + uid)
         .getDownloadURL()
-        .then(function(url) {
-          console.log(url);
-          document.getElementById("profileImg").setAttribute("src", url);
-          return url;
-          // console.log(this.profileUrl);
-          // Get the download URL for 'images/stars.jpg'
-          // This can be inserted into an <img> tag
-          // This can also be downloaded directly
+        .then(url => {
+          this.profileImageUrl = url;
         })
         .catch(function(error) {
-          // Handle any errors
           console.log("error is ", error);
         });
-      console.log(hello, "this is hello");
-      hello.then(function(result) {
-        returnValue = hello;
+    },
+
+    /**
+     * @Method설명 : 문제 검색 테이블의 헤더에 들어갈 안내문 선택 메소드
+     * @변경이력 :
+     */
+    setMessage: function() {
+      if (this.boardType === "answermodify") {
+        this.message = "수정을 요청할 문제 검색";
+      } else if (this.boardType === "question") {
+        this.message = "질문할 문제 검색";
+      }
+    },
+
+    /**
+     * @Method설명 : 게시판 목록으로 돌아가는 메소드
+     * @변경이력 :
+     */
+    goToBoardList: function() {
+      this.$router.push({
+        name: `${this.boardType}-board-list`
       });
-      return returnValue;
     }
   },
-  // 게시판에 따른 안내문구 설정
-  mounted() {
-    if (this.boardType === "answermodify") {
-      this.message = "수정을 요청할 문제 검색";
-    } else if (this.boardType === "question") {
-      this.message = "질문할 문제 검색";
-    }
-    console.log("hello");
-    // this.profileUrl = firebaseSt.ref().child("profileimg/" + this.article.uid);
-    this.profileUrl = this.getProfileImageUrl();
-    console.log(this.getProfileImageUrl(), "why");
+  created() {
+    this.setMessage();
+    this.getProfileImageUrl(SessionStorage.getItem("loginUser").uid);
   }
 };
 </script>
