0f9bab7f419ad66eeabde46e404b264a35c56c54
LEEAYOUNG <souk0711@gmail.com>
2021-01-04 22:08:51 +0900

    [Update]ProblemSetUpdate.vue_코드정리

--- SOLAFY/SOLAFY/src/solafy_vue/src/pages/problemSet/ProblemSetUpdate.vue
+++ SOLAFY/SOLAFY/src/solafy_vue/src/pages/problemSet/ProblemSetUpdate.vue
@@ -5,6 +5,7 @@
       <q-spinner-dots color="primary" size="6em" />
     </div>
     <div class="col-lg-6" v-else>
+      <!-- 문제집 정보 입력 -->
       <div class="row">
         <q-markup-table separator="cell">
           <tbody>
@@ -21,10 +22,13 @@
           </tbody>
         </q-markup-table>
       </div>
+      <!-- 문제집의 총 문제 수 -->
       <div class="row justify-center">
         <h5>총 문제 : {{ item.problemList.length }}</h5>
       </div>
+      <!-- 문제 추가 / 삭제 -->
       <div class="row justify-center">
+        <!-- 문제집에 포함될 문제 리스트 -->
         <div class="col-6">
           <div class="row">
             <q-table
@@ -43,7 +47,7 @@
                     round
                     color="red"
                     icon="remove"
-                    @click="removeProblem(props.rowIndex)"
+                    @click="deleteProblemByBtn(props.rowIndex)"
                   />
                 </q-td>
               </template>
@@ -57,7 +61,7 @@
             />
           </div>
         </div>
-        <q-space />
+        <!-- 문제집에 포함되지 않은 문제 -->
         <div class="col-6">
           <div class="row">
             <q-table
@@ -92,19 +96,20 @@
         </div>
       </div>
       <hr />
+      <!-- 버튼들 -->
       <div class="row">
         <div class="col">
-          <q-btn outline color="primary" label="수정취소" @click="goToproblemSetDetail" />
+          <q-btn outline color="primary" label="수정취소" @click="goToProblemDetailByProblemSetInfo" />
         </div>
         <div class="col">
-          <q-btn color="primary" label="문제수정" @click="UpdateProblemSet"  style="float:right"/>
+          <q-btn color="primary" label="문제수정" @click="updateProblemSet"  style="float:right"/>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-import axios from "axios";
+import Axios from "axios";
 import { firebaseAuth } from "src/boot/firebase";
 import { notify } from "src/api/common.js";
 
@@ -112,16 +117,41 @@ export default {
   name: "ProblemSetCreateByProblemSetInfo",
   data() {
     return {
+      // 로딩 flag
+      loading: true,
+      // 문제집에 포함되지 않은 문제 리스트
+      remainProblemList: [],
+      // 문제집 정보
       item: {
+        // 문제집
         problemSet: {
+          // 문제집 번호
           problemSetNo: 0,
+          // 문제집 제목
           title: "",
+          // 작성자 닉네임
           nickname: ""
         },
+        // 문제 리스트
         problemList: []
       },
-      remainProblemList: [],
+      // 문제집에 포함된 문제 리스트 페이지관련 변수
+      pagination1: {
+        sortBy: "desc",
+        descending: false,
+        page: 1,
+        rowsPerPage: 5
+      },
+      // 문제집에 포함되지않은 문제 리스트 페이지관련 변수
+      pagination2: {
+        sortBy: "desc",
+        descending: false,
+        page: 1,
+        rowsPerPage: 5
+      },
+      // 테이블 컬럼
       columns: [
+        // 문제 번호
         {
           name: "problemNo",
           align: "left",
@@ -130,6 +160,7 @@ export default {
           align: "center",
           style: "width:10vw"
         },
+        // 문제 제목
         {
           name: "title",
           label: "문제 제목",
@@ -137,32 +168,23 @@ export default {
           align: "center",
           style: "width:80vw"
         },
+        // 문제 추가 / 삭제 버튼
         {
           name: "button",
           field: "button",
           align: "center",
           style: "width:10vw"
         }
-      ],
-      pagination1: {
-        sortBy: "desc",
-        descending: false,
-        page: 1,
-        rowsPerPage: 5
-      },
-      pagination2: {
-        sortBy: "desc",
-        descending: false,
-        page: 1,
-        rowsPerPage: 5
-      },
-      loading: true
+      ]
     };
   },
   methods: {
-    // 문제집 정보 반환
-    selectProblemSet() {
-      axios
+    /**
+     * @Method설명 : 문제집 정보 반환
+     * @변경이력 :
+     */
+    selectProblemSetByNo() {
+      Axios
         .get(
           "problem/problemset/problemSetSelectByNo/" +
             this.$route.params.problemSetNo
@@ -172,14 +194,17 @@ export default {
         })
         .catch(error => {
           notify("negative", "white", "error", "조회 실패");
-          this.goToproblemSetDetail();
+          this.goToProblemDetailByProblemSetInfo();
           console.log(error);
         })
         .finally(() => {});
     },
-    // 남은 문제 정보 반환
+    /**
+     * @Method설명 : 문제집에 추가되지않은 문제 정보 반환
+     * @변경이력 :
+     */
     selectProblemList() {
-      axios
+      Axios
         .get("problem")
         .then(response => {
           // 이미 선택되어 있는 문제는 제거
@@ -193,15 +218,18 @@ export default {
         })
         .catch(error => {
           notify("negative", "white", "error", "조회 실패");
-          this.goToproblemSetDetail();
+          this.goToProblemDetailByProblemSetInfo();
           console.log(error);
         })
         .finally(() => {
           this.loading = false;
         });
     },
-    // 문제집 상세보기로 이동
-    goToproblemSetDetail() {
+    /**
+     * @Method설명 : 문제집 상세보기로 이동
+     * @변경이력 :
+     */
+    goToProblemDetailByProblemSetInfo() {
       this.$router.push({
         name: "ProblemDetailByProblemSetInfo",
         params: {
@@ -209,7 +237,10 @@ export default {
         }
       });
     },
-    // 문제리스트에서 문제 제거
+    /**
+     * @Method설명 : 문제집 문제리스트에서 문제 제거
+     * @변경이력 :
+     */
     removeProblem(index) {
       this.remainProblemList.push(this.item.problemList[index]);
       this.item.problemList.splice(index, 1);
@@ -221,7 +252,10 @@ export default {
         return p1.problemNo - p2.problemNo;
       });
     },
-    // 문제리스트에 문제 추가
+    /**
+     * @Method설명 : 문제리스트에 문제 추가
+     * @변경이력 :
+     */
     addProblem(index) {
       this.item.problemList.push(this.remainProblemList[index]);
       this.remainProblemList.splice(index, 1);
@@ -233,8 +267,11 @@ export default {
         return p1.problemNo - p2.problemNo;
       });
     },
-    // 문제 수정 버튼 클릭 시 db에 반영
-    UpdateProblemSet() {
+    /**
+     * @Method설명 : 수정사항을 DB에 반영
+     * @변경이력 :
+     */
+    updateProblemSet() {
       // 예외 처리
       if (this.item.problemSet.title == "") {
         notify("warning", "white", "warning", "문제집 제목을 입력해주세요");
@@ -250,12 +287,12 @@ export default {
         return;
       }
       // 문제 수정
-      axios
+      Axios
         .put("problem/problemset/updateProblemSet", this.item)
         .then(response => {
           if ((response.data = "success")) {
             notify("positive", "white", "done", "수정 성공");
-            this.goToproblemSetDetail();
+            this.goToProblemDetailByProblemSetInfo();
           } else {
             notify("negative", "white", "error", "수정 실패");
           }
@@ -265,10 +302,6 @@ export default {
           console.log(error);
         });
     }
-  },
-  created() {
-    this.selectProblemSet();
-    this.selectProblemList();
   },
   computed: {
     pagesNumber1() {
@@ -281,7 +314,11 @@ export default {
         this.remainProblemList.length / this.pagination2.rowsPerPage
       );
     }
-  }
+  },
+  created() {
+    this.selectProblemSetByNo();
+    this.selectProblemList();
+  },
 };
 </script>
 <style scoped>
