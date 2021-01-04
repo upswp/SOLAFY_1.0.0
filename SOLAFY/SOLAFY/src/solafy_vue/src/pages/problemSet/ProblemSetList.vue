adc6065f6a96dc44e280ea9573bfd92a83879df7
LEEAYOUNG <souk0711@gmail.com>
2021-01-04 20:16:29 +0900

    [Update]ProblemSetList.vue_코드정리

--- SOLAFY/SOLAFY/src/solafy_vue/src/pages/problemSet/ProblemSetList.vue
+++ SOLAFY/SOLAFY/src/solafy_vue/src/pages/problemSet/ProblemSetList.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="q-pa-md">
     <div class="column" style="height: 150px">
+      <!-- 버튼들 -->
       <div class="col">
-        <!-- onclick="location.reload() ::: 클릭 시 새로고침 -->
         <q-btn
           color="primary"
           :disable="loading"
           label="문제 List"
-          @click="problemList"
+          @click="goToProblem"
         />
+        <!-- onclick="location.reload() ::: 클릭 시 새로고침 -->
         <q-btn
           class="q-ml-sm"
           color="primary"
@@ -22,20 +23,21 @@
           color="primary"
           :disable="loading"
           label="문제집 등록"
-          @click="problemSetCreate"
+          @click="goToProblemSetCreateByProblemSetInfo"
         />
       </div>
+      <!-- 문제집 리스트 테이블 -->
       <div class="col">
         <q-table
           title="문제집 리스트"
           :data="problemSetList"
           :columns="columns"
           row-key="name"
-          :filter="filter"
           :loading="loading"
-          @row-click="problemSetDetail"
+          @row-click="goToProblemDetailByProblemSetInfo"
           no-data-label="검색결과가 없습니다"
         >
+          <!-- 검색바 -->
           <template v-slot:top-right>
             <q-input
               bottom-slots
@@ -65,7 +67,7 @@
               </template>
             </q-input>
           </template>
-
+          <!-- 테이블에 데이터가 없을 때 -->
           <template v-slot:no-data="{ message }">
             <div class="full-width row flex-center text-accent q-gutter-sm">
               <q-icon size="2em" name="sentiment_dissatisfied" />
@@ -79,22 +81,26 @@
 </template>
 
 <script>
-import axios from "axios";
+import Axios from "axios";
 import routes from "src/router/routes";
 import { notify } from "src/api/common";
 
 export default {
   data() {
     return {
+      // 로딩 flag
       loading: false,
-      filter: "",
-      categoryNo: "",
-      rowCount: 10,
-      errored: false,
-      keyword: null,
+      // 검색 키워드
+      keyword: "",
+      // 검색 옵션
       option: "제목",
+      // 문제집 리스트
+      problemSetList: [],
+      // select에 들어가는 검색 옵션
       options: ["제목", "문제집번호", "작성자"],
+      // 테이블 컬럼
       columns: [
+        // 문제집 번호
         {
           name: "problemSetNo",
           required: true,
@@ -104,6 +110,7 @@ export default {
           sortable: true,
           style: "width = 10px"
         },
+        // 제목
         {
           name: "title",
           label: "제목",
@@ -112,6 +119,7 @@ export default {
           field: "title",
           sortable: true
         },
+        // 작성자
         {
           name: "nickname",
           label: "작성자",
@@ -120,6 +128,7 @@ export default {
           field: "nickname",
           sortable: true
         },
+        // 작성일자
         {
           name: "regiTime",
           label: "작성일자",
@@ -128,15 +137,17 @@ export default {
           field: "regiTime",
           sortable: true
         }
-      ],
-      problemSetList: []
+      ]
     };
   },
   methods: {
-    //문제집 전체 리스트 반환
-    selectProblemSetList() {
+    /**
+     * @Method설명 : 문제집 전체 리스트 반환
+     * @변경이력 :
+     */
+    selectAllProblemSet() {
       this.loading = true;
-      axios
+      Axios
         .get("problem/problemset/problemSetSelect")
         .then(response => {
           this.problemSetList = response.data;
@@ -149,24 +160,30 @@ export default {
           this.loading = false;
         });
     },
-    //문제 검색 및 검색 예외처리
+    /**
+     * @Method설명 : 문제 검색 및 검색 예외처리
+     * @변경이력 :
+     */
     searchProblemSet() {
       this.loading = true;
       if(this.keyword==""){
-        this.selectProblemSetList();
+        this.selectAllProblemSet();
         return;
       }
       if (this.option == "제목") {
-        this.selectProblemByTitle();
+        this.selectProblemSetByTitle();
       } else if (this.option == "문제집번호") {
-        this.selectProblemByNo();
+        this.selectProblemSetByNo();
       } else {
-        this.selectProblemByWriter();
+        this.selectProblemSetByWriter();
       }
     },
-    // 문제집 제목으로 검색
-    selectProblemByTitle() {
-      axios
+    /**
+     * @Method설명 : 문제집 제목으로 문제집 검색
+     * @변경이력 :
+     */
+    selectProblemSetByTitle() {
+      Axios
         .get("problem/problemset/problemSetSelectByTitle/" + this.keyword)
         .then(response => {
           this.problemSetList = response.data;
@@ -176,9 +193,12 @@ export default {
         })
         .finally(() => (this.loading = false));
     },
-    // 문제집 번호로 검색
-    selectProblemByNo() {
-      axios
+    /**
+     * @Method설명 : 문제집 번호로 문제집 검색
+     * @변경이력 :
+     */
+    selectProblemSetByNo() {
+      Axios
         .get("problem/problemset/problemSetSelectByNo/" + this.keyword)
         .then(response => {
           this.problemSetList=[];
@@ -189,9 +209,12 @@ export default {
         })
         .finally(() => (this.loading = false));
     },
-    // 문제집 작성자로 검색
-    selectProblemByWriter() {
-      axios
+    /**
+     * @Method설명 : 문제집 작성자로 문제집검색
+     * @변경이력 :
+     */
+    selectProblemSetByWriter() {
+      Axios
         .get("problem/problemset/problemSetSelectByWriter/" + this.keyword)
         .then(response => {
           this.problemSetList = response.data;
@@ -201,21 +224,30 @@ export default {
         })
         .finally(() => (this.loading = false));
     },
-    // problemList로 이동
-    problemList() {
+    /**
+     * @Method설명 : 문제 리스트로 이동
+     * @변경이력 :
+     */
+    goToProblem() {
       this.loading = true;
       this.$router.push({
         name: "Problem"
       });
     },
-    //problemSetCreate page로 이동
-    problemSetCreate() {
+    /**
+     * @Method설명 : 문제집 생성페이지로 이동
+     * @변경이력 :
+     */
+    goToProblemSetCreateByProblemSetInfo() {
       this.$router.push({
         name: "ProblemSetCreateByProblemSetInfo"
       });
     },
-    //problemSetDetail page로 이동
-    problemSetDetail(evt, row) {
+    /**
+     * @Method설명 : 문제집 상세페이지로 이동
+     * @변경이력 :
+     */
+    goToProblemDetailByProblemSetInfo(evt, row) {
       this.loading = true;
       this.$router.push({
         name: "ProblemDetailByProblemSetInfo",
@@ -226,7 +258,7 @@ export default {
     }
   },
   created() {
-    this.selectProblemSetList();
+    this.selectAllProblemSet();
   }
 };
 </script>
